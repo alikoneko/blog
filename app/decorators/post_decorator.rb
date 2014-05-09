e2ceec85d5b2880f8018d9f82981ac2c6b397f02
Alison Neer <alison.neer@gmail.com>
2014-05-09 17:32:17 -0400

    tag_list for posts decorated

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -8,7 +8,19 @@ class PostDecorator < Draper::Decorator
   def render_created_at
     h.content_tag :span, object.created_at, class: 'time zone unprocessed', data: { utc: object.created_at.to_i }
   end
+
   def render_updated_at
     h.content_tag :span, object.updated_at, class: 'time zone unprocessed', data: { utc: object.updated_at.to_i }
   end
+
+  def render_tag_list
+    h.capture do
+      object.tags.each do |tag|
+        h.concat(h.content_tag(:span, class: 'label label-tags') {
+          h.link_to(tag.name, h.polymorphic_path([:posts], tag: tag.name))
+        })
+      end
+    end
+  end
+
 end
