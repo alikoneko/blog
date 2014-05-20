d3c99857aabbce62569c72939c0d5f6b286fe3ec
Alison Neer <alison.neer@gmail.com>
2014-05-20 13:01:02 -0400

    with invalid credentials

--- spec/controllers/sessions/sessions_spec.rb
+++ spec/controllers/sessions/sessions_spec.rb
@@ -10,5 +10,26 @@ describe SessionsController do
       end
     end
 
+    describe "#create" do
+      let(:user) { create :user }
+
+      context "with valid credentials" do
+        it "assigns the user id to the current session " do
+          post :create, session_form: { email: user.email, password: user.password }
+
+          expect(session[:user_id]).to eq(user.id)
+        end
+      end
+
+      context "without valid credentials" do
+        it "assigns the @session_form" do
+          post :create, session_form: { email: user.email, password: "tits" }
+
+          expect(assigns(:session_form)).to be_present
+        end
+      end
+    end
+
+
   end
 end
